2bc33f79fab7db10f211beb478b541d731a69ec0
arevbond <bondarev.n17@yandex.ru>
2023-03-30 13:06:04 +0300

    Add 5 tests about users

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -3,19 +3,18 @@
 """
 
 from conftest import client
-import time
 
 
 class TestUserAuthentication:
-    def test_register_and_login_client(self):
+    def test_register_client(self):
         response = client.post("/auth/register",
                                json={
                                    "email": "user@example.com",
-                                   "password": "string",
+                                   "password": "example_password",
                                    "is_active": True,
                                    "is_superuser": False,
                                    "is_verified": False,
-                                   "username": "string"
+                                   "username": "user_example1"
                                })
 
         assert response.status_code == 201
@@ -24,16 +23,47 @@ class TestUserAuthentication:
         assert data["is_active"] is True
         assert data["is_superuser"] is False
         assert data["is_verified"] is False
-        assert data["username"] == "string"
+        assert data["username"] == "user_example1"
         assert "password" not in data
         assert "hashed_password" not in data
-        client.post("/login")
 
-        response = client.post("/auth/jwt/login", data={"username": "user@example.com", "password": "string"})
+    def test_login_client(self):
+        response = client.post("/auth/jwt/login", data={"username": "user@example.com", "password": "example_password"})
         assert response.status_code == 200
         data = response.json()
         assert "access_token" in data and data["token_type"] == "bearer"
+
+    def test_authenticated_authorized_client(self):
+        response = client.post("/auth/jwt/login", data={"username": "user@example.com", "password": "example_password"})
+        data = response.json()
         access_token = data["access_token"]
-        # response = client.get("/authenticated-route")
-        # assert response.status_code == 200
-        # print(response.text)
+        response = client.get("/authenticated-route", headers={"Authorization": f"Bearer {access_token}"})
+        assert response.status_code == 200
+
+    def test_authenticated_not_authorized_client(self):
+        response = client.get("/authenticated-route")
+        assert response.status_code == 401
+        assert response.json().get("detail") == "Unauthorized"
+
+    def test_user_get_himself(self):
+        client.post("/auth/register",
+                    json={
+                        "email": "user1@example.com",
+                        "password": "user_example2",
+                        "is_active": True,
+                        "is_superuser": False,
+                        "is_verified": False,
+                        "username": "user_example2"
+                    })
+
+        response = client.post("/auth/jwt/login", data={"username": "user1@example.com", "password": "user_example2"})
+        data = response.json()
+        access_token = data["access_token"]
+
+        response = client.get("/users/me", headers={"Authorization": f"Bearer {access_token}"})
+        data = response.json()
+        assert response.status_code == 200
+        assert "id" in data
+        assert data.get("email") == "user1@example.com"
+        assert data.get("username") == "user_example2"
+        assert "password" not in data or "hashed_password" not in data
